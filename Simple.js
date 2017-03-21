2909c54a090c37831e4e7165ab908d025bfbb455
hanai <ihanai1991@gmail.com>
2017-03-21 12:01:13 +0800

    UPDATE

--- Simple.js
+++ Simple.js
@@ -12,10 +12,6 @@ Number.prototype = {
   }
 };
 
-Number.new = function(...args) {
-  return new Number(...args);
-};
-
 function Add(left, right) {
   this.left = left;
   this.right = right;
@@ -30,20 +26,16 @@ Add.prototype = {
   inspect() {
     return `«${this}»`;
   },
-  reduce() {
+  reduce(env) {
     const {left, right} = this;
     if (left.reducible) {
-      return new Add(left.reduce(), right);
+      return Add.new(left.reduce(env), right);
     } else if (right.reducible) {
-      return new Add(left, right.reduce());
+      return Add.new(left, right.reduce(env));
     } else {
-      return new Number(left.value + right.value);
+      return Number.new(left.value + right.value);
     }
   }
-};
-
-Add.new = function(...args) {
-  return new Add(...args);
 };
 
 function Multiply(left, right) {
@@ -60,24 +52,151 @@ Multiply.prototype = {
   inspect() {
     return `«${this}»`;
   },
-  reduce() {
+  reduce(env) {
     const {left, right} = this;
     if (left.reducible) {
-      return new Multiply(left.reduce(), right);
+      return Multiply.new(left.reduce(env), right);
     } else if (right.reducible) {
-      return new Multiply(left, right.reduce());
+      return Multiply.new(left, right.reduce(env));
     } else {
-      return new Number(left.value * right.value);
+      return Number.new(left.value * right.value);
     }
   }
 };
 
-Multiply.new = function(...args) {
-  return new Multiply(...args);
+function Boolean(value) {
+  this.value = value;
+}
+
+Boolean.prototype = {
+  reducible: false,
+  toString() {
+    return this.value.toString();
+  },
+  inspect() {
+    return `«${this}»`;
+  }
 };
+
+function LessThan(left, right) {
+  this.left = left;
+  this.right = right;
+}
+
+LessThan.prototype = {
+  reducible: true,
+  toString() {
+    const {left, right} = this;
+    return `${left} < ${right}`;
+  },
+  inspect() {
+    return `«${this}»`;
+  },
+  reduce(env) {
+    const {left, right} = this;
+    if (left.reducible) {
+      return LessThan.new(left.reduce(env), right);
+    } else if (right.reducible) {
+      return LessThan.new(left, right.reduce(env));
+    } else {
+      return Boolean.new(left.value < right.value);
+    }
+  }
+};
+
+function Variable(name) {
+  this.name = name;
+}
+
+Variable.prototype = {
+  reducible: true,
+  toString() {
+    return this.name.toString();
+  },
+  inspect() {
+    return `«${this}»`;
+  },
+  reduce(env) {
+    return env[this.name];
+  }
+};
+
+function Assign(name, exp) {
+  this.name = name;
+  this.exp = exp;
+}
+
+Assign.prototype = {
+  reducible: true,
+  toString() {
+    const {name, exp} = this;
+    return `${name} = ${exp}`;
+  },
+  inspect() {
+    return `«${this}»`;
+  },
+  reduce(env) {
+    const {name, exp} = this;
+    if (exp.reducible) {
+      return [Assign.new(name, exp.reduce(env)), env];
+    } else {
+      const e = {};
+      e[name] = exp;
+      return [DoNothing.new(), Object.assign({}, env, e)];
+    }
+  }
+}
+
+function DoNothing() {
+}
+
+DoNothing.prototype = {
+  reducible: false,
+  toString() {
+    return 'do-nothing';
+  },
+  inspect() {
+    return `«${this}»`;
+  },
+  equal(state) {
+    return state instanceof DoNothing;
+  }
+}
+
+function Machine(state, env) {
+  this.state = state;
+  this.env = env;
+}
+
+Machine.prototype = {
+  step() {
+    [this.state, this.env ] = this.state.reduce(this.env);
+  },
+  run() {
+    while (this.state.reducible) {
+      console.log(`${this.state}, ${this.env}`);
+      this.step();
+    }
+    console.log(`${this.state}, ${this.env}`);
+  }
+};
+
+[Number, Add, Multiply, Boolean,
+  LessThan, Machine, Variable, Assign,
+  DoNothing].forEach(cls => {
+    cls.new = function(...args) {
+      return new cls(...args);
+    };
+  });
 
 module.exports = {
   Number,
   Add,
-  Multiply
+  Multiply,
+  Boolean,
+  LessThan,
+  Variable,
+  DoNothing,
+  Assign,
+  Machine
 };
